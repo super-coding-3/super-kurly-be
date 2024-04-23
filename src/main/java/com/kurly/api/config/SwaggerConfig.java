efc442926adb66e66e9659c36d14ce496c503f81
Hagjoon <105399835+leehagjoon@users.noreply.github.com>
2024-04-24 02:37:19 +0900

    [feature] : Swagger 생성

---
+++ src/main/java/com/kurly/api/config/SwaggerConfig.java
@@ -0,0 +1,46 @@
+package com.kurly.api.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Arrays;
+
+/**
+ * packageName    : com.kurly.api.config
+ * fileName       : SwaggerConfig
+ * author         : hagjoon
+ * date           : 2024-04-24
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2024-04-24        hagjoon       최초 생성
+ */
+@OpenAPIDefinition(
+        info = @Info(title = "super-kurly API 명세서",
+        description = "슈퍼컬리 API 명세서",
+        version = "v1")
+)
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI(){
+        SecurityScheme securityScheme = new SecurityScheme()
+                .type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")
+                .in(SecurityScheme.In.HEADER).name("Authorization");
+        SecurityRequirement securityRequirement = new SecurityRequirement().addList("bearerAuth");
+
+        return new OpenAPI()
+                .components(new Components().addSecuritySchemes("bearerAuth",securityScheme))
+                .security(Arrays.asList(securityRequirement));
+    }
+
+}
