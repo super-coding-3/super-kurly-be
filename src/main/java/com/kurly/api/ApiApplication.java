7ad070b77f21c7ecd5a5ad40597cd0a97d80d2df
Hagjoon <105399835+leehagjoon@users.noreply.github.com>
2024-04-30 21:39:46 +0900

    [feature] : 임시 s3 yml 제거

--- src/main/java/com/kurly/api/ApiApplication.java
+++ src/main/java/com/kurly/api/ApiApplication.java
@@ -2,22 +2,14 @@ package com.kurly.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
 @SpringBootApplication
 public class ApiApplication {
 
-    public static final String APPLICATION_LOCATIONS = "spring.config.location="
-            + "classpath:application.yml,"
-            + "classpath:application-local.yml,"
-            + "classpath:application-prod.yml";
-
     public static void main(String[] args) {
-        new SpringApplicationBuilder(ApiApplication.class)
-                .properties(APPLICATION_LOCATIONS)
-                .run(args);
+        SpringApplication.run(ApiApplication.class, args);
     }
 
 }
